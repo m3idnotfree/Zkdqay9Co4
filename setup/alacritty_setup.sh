cb172e2281cbc065b0b4dc9234b91d26f49a99f4
m3id <m3id.eprac@gmail.com>
2024-06-23 09:00:30 +0900

    refactor(setup/alacritty):

--- setup/alacritty_setup.sh
+++ setup/alacritty_setup.sh
@@ -1,22 +1,8 @@
 #! /bin/bash
 
-# alacritty="$HOME/.config/alacritty"
-#
-# save_yn=$1
-# config_path=$2
-# backup_path=$3
+alacritty_setup() {
+	move_backup_dir $ALACRITTY_PATH
+	cp_config_dir $ALACRITTY_PATH ./alacritty
+}
 
-if [ "$yn" == "y" ]; then
-	# if [ -d "$alacritty" ]; then
-	# 	echo "alacritty config already exist"
-	# 	echo "mv ~/.config/alacritty ~/zkd/alacritty"
-	#
-	# 	mv $alacritty $backup_path
-	# fi
-	move_backup $alacritty_path
-fi
-
-# echo "create alacritty config"
-# mkdir -p $alacritty
-# cp -rf ../alacritty $config_path
-cp_config $alacritty_path ./alacritty
+alacritty_setup

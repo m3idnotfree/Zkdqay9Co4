ae39ad443f8de067f88ce07fb67eb2e251ca26b2
m3id <m3id.eprac@gmail.com>
2024-06-23 09:01:36 +0900

    refactor(setup/starship):

--- setup/starship_setup.sh
+++ setup/starship_setup.sh
@@ -1,19 +1,8 @@
 #! /bin/bash
-# startship="$HOME/.config/starship.toml"
-#
-# save_yn=$1
-# config_path=$2
-# back_path=$3
 
-if [ "$yn" == "y" ]; then
-	# if [ -f "$starship" ]; then
-	# 	echo "mv ~/.config/starship.toml ~/zkd/starship.toml"
-	#
-	# 	mv $starship $back_path
-	# fi
-	f_move_backup $starship_path
-fi
+starship_setup() {
+	move_backup_file $STARSHIP_PATH
+	cp_config_file "./starship.toml"
+}
 
-# echo "create starship config"
-# cp -f ../starship.toml $config_path
-f_cp_config "./starship.toml"
+starship_setup

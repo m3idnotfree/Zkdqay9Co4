4f4c53f892e46f76fa485c43998bb456c8ff063f
m3id <m3id.eprac@gmail.com>
2024-06-23 09:02:12 +0900

    deleted():

--- arch/yay.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-yay_install() {
-	if ! command -v yay &>/dev/null; then
-		echo "install yay"
-		command sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
-	fi
-}

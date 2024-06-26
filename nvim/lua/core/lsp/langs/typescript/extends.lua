8b15234565a1ba8b74b70ad8f8f40c6def7b2ac4
m3id <m3id.eprac@gmail.com>
2024-06-26 23:48:03 +0900

    feat(nvim): core/lsp/typescript extends

---
+++ nvim/lua/core/lsp/langs/typescript/extends.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    'axelvc/template-string.nvim',
+    ft = {
+      'html',
+      'typescript',
+      'javascript',
+      'typescriptreact',
+      'javascriptreact',
+      'python',
+    },
+    opts = {},
+  },
+  {
+    'dmmulroy/ts-error-translator.nvim',
+    ft = { 'javascript', 'typescript', 'javascriptreact', 'typescriptreact' },
+    opts = {
+      -- auto_override_publish_diagnostics = true,
+    },
+  },
+}

460deb87b215d405623e4f4085a48af157170014
m3id <m3id.eprac@gmail.com>
2024-06-26 23:22:20 +0900

    feat(nvim): config/colors

---
+++ nvim/lua/config/colors.lua
@@ -0,0 +1,14 @@
+return {
+  white = '#dcd7ba',
+  fg0 = '#d4be98',
+  fg1 = '#ddc7a1',
+  orange = '#e78a4e',
+  yellow_ocher = '#d8a657',
+  yellow = '#fabd2f',
+  magenta = '#fc56B1',
+  light_grey = '#dbdbdb',
+  wine = '#ba508a',
+  prussian_blue = '#0f3a42',
+  dark_brown = '#420f42',
+  sepia = '#574833',
+}
